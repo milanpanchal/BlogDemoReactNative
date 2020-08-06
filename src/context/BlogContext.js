99ce79fdcfaa2a34acd07f2ed59e9f93704fdd9c
Milan Panchal <sam07it22@gmail.com>
2020-08-06 10:48:56 +0530

    feat: Automatic Context Creation

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,8 +1,7 @@
-import React, { useReducer } from 'react';
+import createDataContext from './createDataContext';
 
-const BlogContext = React.createContext();
 
-const blogReducer(state, action) => {
+const blogReducer = (state, action) => {
 	switch (action.type) {
 		case 'add_blogpost':
 			return [...state, { title: `Blog Post #${ state.length + 1 }` }];
@@ -13,15 +12,12 @@ const blogReducer(state, action) => {
 	}
 };
 
-export const BlogProvider = ({ children }) => {
-	// const blogPosts = [ { title: 'Blog Post #1' }, { title: 'Blog Post #2' }, { title: 'Blog Post #3' }, { title: 'Blog Post #4' }, { title: 'Blog Post #5' } ];
-	const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-
-	return <BlogContext.Provider value={{data: blogPosts}}>  
-		{ children }
-	</BlogContext.Provider>
-
+const addBlogPost = (dispatch) => {
+	return () => {
+		dispatch({ type: 'add_blogpost'});
+	};
+	
 };
 
-export default BlogContext;
+
+export const { Context, Provider } = createDataContext(blogReducer, {addBlogPost}, []);
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,18 +1,17 @@
 import React, { useContext } from 'react';
 import { Text, View, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
-import BlogContext from '../context/BlogContext';
+import { Context } from '../context/BlogContext';
 
 const IndexScreen = () => {
 
-	const { data, addBlogPost } = useContext(BlogContext)
-	console.log(data)
+	const { state, addBlogPost } = useContext(Context)
 	return (
 		<View>			
 			<TouchableOpacity activeOpacity = { .8 } style={styles.buttonStyle} onPress={addBlogPost}>
 				<Text style={styles.buttonTitleStyle}> Add New Blog</Text>
 			</TouchableOpacity>
 			<FlatList 
-				data={data} 
+				data={state} 
 				keyExtractor={(blogPost) => blogPost.title} 
 				renderItem={( { item }) => {
 					return <Text style={styles.titleStyle}>{item.title}</Text>
