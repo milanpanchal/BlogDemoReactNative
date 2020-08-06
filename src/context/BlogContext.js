40082efb5b784247503dbe6c01b498f243ac4b11
Milan Panchal <sam07it22@gmail.com>
2020-08-06 10:23:34 +0530

    feat: Context Creation

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
+const blogReducer(state, action) => {
+	switch (action.type) {
+		case 'add_blogpost':
+			return [...state, { title: `Blog Post #${ state.length + 1 }` }];
+		case 'delete_blogpost':
+			return [...state, { title: `Blog Post #${ state.length + 1 }` }];
+		default:
+			return state;
+	}
+};
+
 export const BlogProvider = ({ children }) => {
 	// const blogPosts = [ { title: 'Blog Post #1' }, { title: 'Blog Post #2' }, { title: 'Blog Post #3' }, { title: 'Blog Post #4' }, { title: 'Blog Post #5' } ];
-	const [blogPosts, setBlogPosts] = useState([]);
+	const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
-	const addBlogPost = () => {
-		setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` } ]);
-	};
 
-	return <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>  
+	return <BlogContext.Provider value={{data: blogPosts}}>  
 		{ children }
 	</BlogContext.Provider>
 
